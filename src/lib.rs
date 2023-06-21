ba12913dba6d8cc069544fb382365d23b0abc9f0
SNIAPA <wasilewskihuberts@gmail.com>
2023-06-22 00:26:06 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::thread;
+use std::{thread, error::Error, fs::OpenOptions};
+use libloading::{self, os::unix::Library};
 
 use ctor::{dtor,ctor};
 
@@ -8,16 +9,36 @@ use logger::Logger;
 
 static mut LOGGER: Option<Logger> = None;
 
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
 #[ctor]
 fn load(){
     thread::spawn(|| {
         unsafe {
             LOGGER = Some(Logger::new().expect("failed to init logger"));
+            
         }
+        if let Err(e) = main() {
+            log!("{}",e);
+            unload();
+            load();
+        }
+
     });
 }
 
+fn main() -> Result<()>{
+
+
+    Library::this().close().unwrap();
+    log!("test");
+
+    Ok(())
+}
+
+#[dtor]
 fn unload(){ 
+    log!("unloading");
 
     unsafe{
         LOGGER.as_mut().unwrap().console.kill().unwrap();
--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,8 +1,9 @@
-use std::{fs::{File, create_dir}, io::Write, process::{Child, Command}, path::Path};
+use std::{fs::{File, create_dir, OpenOptions}, io::Write, process::{Child, Command, Stdio}, path::Path};
 use chrono::Utc;
 
-static LOG_DIR: &str = "/tmp/ACC/";
-static INIT_MESSAGE: &str = "░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
+static LOG_DIR: &str = "/tmp/ACI/";
+static INIT_MESSAGE: &str = r#"
+░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░░
 ░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░
 ░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░
 ░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░
@@ -16,12 +17,32 @@ static INIT_MESSAGE: &str = "░░░░░▄▄▄▄▀▀▀▀▀▀▀▀
 ░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░
 ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░
 ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░
-░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░";
+░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░
+ _____ _                  _          _____         _ _ 
+|_   _(_)                | |        |_   _|       | | |
+  | |  _ _ __ ___   ___  | |_ ___     | |_ __ ___ | | |
+  | | | | '_ ` _ \ / _ \ | __/ _ \    | | '__/ _ \| | |
+  | | | | | | | | |  __/ | || (_) |   | | | | (_) | | |
+  \_/ |_|_| |_| |_|\___|  \__\___/    \_/_|  \___/|_|_|
+"#;
 
+#[macro_export]
 macro_rules! log {
-    ($message: expr) => {
+
+    ($($message: expr),*) => {
+        use chrono::Utc;
+
         unsafe{
-            crate::LOGGER.as_mut().unwrap().log($message).unwrap();
+            let msg = &format!($($message),*);
+            let timestamp = Utc::now().format("%Y-%m-%d %H:%M:%S");
+            let line = std::line!();
+            let file = std::file!();
+
+            crate::LOGGER
+                .as_mut()
+                .unwrap()
+                .log(&format!("[{}] {}:{}\n {}",timestamp,file,line,msg))
+                .unwrap();
         }
     };
 }
@@ -32,13 +53,15 @@ pub struct Logger{
 }
 impl Logger {
     pub fn new() -> Result<Logger, std::io::Error> {
-        let file_name = format!("{}-{}.log", LOG_DIR, Utc::now().format("%s"));
+        let file_name = format!("{}log_{}.log", LOG_DIR, Utc::now().format("%Y-%m-%d %H:%M:%S"));
 
         if !(Path::new(LOG_DIR).is_dir()){
             create_dir(LOG_DIR)?;
         }
         let file = File::create(&file_name)?;
-        let console = Command::new("alacritty").args(["-e", "tail","-n","100000", "-f", &file_name]).spawn()?;
+        let console = Command::new("alacritty")
+            .args(["-e", "tail","-n","100000", "-f", &file_name])
+            .spawn()?;
 
         let mut logger = Logger {file, console};
         logger.log(INIT_MESSAGE)?;
@@ -52,3 +75,4 @@ impl Logger {
 
     
 }
+
